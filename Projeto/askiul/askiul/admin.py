c3057b862d1a9cbcb607db98ebcf47860b182ca4
Bernardo Santos <bmrss@iscte-iul.pt>
2022-05-12 17:07:40 +0100

    Foreign Key

--- Projeto/askiul/askiul/admin.py
+++ Projeto/askiul/askiul/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Cadeira, Curso
+from .models import Cadeira, Curso, Associar
 
 
 admin.site.register(Cadeira)
 admin.site.register(Curso)
+admin.site.register(Associar)
--- Projeto/askiul/askiul/models.py
+++ Projeto/askiul/askiul/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Cadeira(models.Model):
     name = models.CharField(max_length=200)
     descrição = models.CharField(max_length=200)
-    
     def __str__(self):
         return self.name
-
+    
+    
 
 
 class Curso(models.Model):
@@ -14,3 +14,7 @@ class Curso(models.Model):
     Sigla = models.CharField(max_length=10,unique=True)
     def __str__(self):
         return self.Sigla
+
+class Associar(models.Model):
+    Curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    Cadeira = models.ForeignKey(Cadeira, on_delete=models.CASCADE)
